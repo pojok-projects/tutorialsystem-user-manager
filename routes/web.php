c8ba7070db60ac580e78d9a9a5504fcd49684bb1
vibi <vibi94@gmail.com>
2019-08-19 16:31:51 +0700

    UPA-43 feat(User Profile API): Added Controller UserDislikeVideo

--- routes/web.php
+++ routes/web.php
@@ -22,35 +22,41 @@ $router->group(['prefix'=>'v1'], function() use($router){
     $router->get('/user/following', 'UserFollowingController@index');
     $router->get('/user/follower', 'UserFollowerController@index');
     $router->get('/user/likevideo', 'UserLikeVideoController@index');
+    $router->get('/user/dislikevideo', 'UserDislikeVideoController@index');
 
     // Show Routes
     $router->get('user/{id}', 'UserController@show');
     $router->get('user/following/{id}', 'UserFollowingController@show');
     $router->get('user/follower/{id}', 'UserFollowerController@show');
     $router->get('user/likevideo/{id}', 'UserLikeVideoController@show');
+    $router->get('user/dislikevideo/{id}', 'UserDislikeVideoController@show');
 
     // Store Routes
     $router->post('/user/store', 'UserController@create');
     $router->post('/user/following/store', 'UserFollowingController@create');
     $router->post('/user/follower/store', 'UserFollowerController@create');
     $router->post('/user/likevideo/store', 'UserLikeVideoController@create');
+    $router->post('/user/dislikevideo/store', 'UserDislikeVideoController@create');
 	
 	// Search Routes
 	$router->post('/user/search', 'UserController@search');
 	$router->post('/user/following/search', 'UserFollowingController@search');
 	$router->post('/user/follower/search', 'UserFollowerController@search');
 	$router->post('/user/likevideo/search', 'UserLikeVideoController@search');
+	$router->post('/user/dislikevideo/search', 'UserDislikeVideoController@search');
 
 	// Update Routes
 	$router->put('/user/update/{id}', 'UserController@update');
 	$router->put('/user/following/update/{id}', 'UserFollowingController@update');
 	$router->put('/user/follower/update/{id}', 'UserFollowerController@update');
 	$router->put('/user/likevideo/update/{id}', 'UserLikeVideoController@update');
+	$router->put('/user/dislikevideo/update/{id}', 'UserDislikeVideoController@update');
 
 	// Delete Routes
 	$router->delete('/user/delete/{id}', 'UserController@destroy');
 	$router->delete('/user/following/delete/{id}', 'UserFollowingController@destroy');
 	$router->delete('/user/follower/delete/{id}', 'UserFollowerController@destroy');
 	$router->delete('/user/likevideo/delete/{id}', 'UserLikeVideoController@destroy');
+	$router->delete('/user/dislikevideo/delete/{id}', 'UserDislikeVideoController@destroy');
 
 });
