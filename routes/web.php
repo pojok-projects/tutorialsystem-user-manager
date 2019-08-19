98e0cf1375f74665060577073a9046870ebee5e2
vibi <vibi94@gmail.com>
2019-08-19 16:46:20 +0700

    UPA-43 feat(User Profile API): Added Controller UserSavedVideo

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ $router->group(['prefix'=>'v1'], function() use($router){
     $router->get('/user/follower', 'UserFollowerController@index');
     $router->get('/user/likevideo', 'UserLikeVideoController@index');
     $router->get('/user/dislikevideo', 'UserDislikeVideoController@index');
+    $router->get('/user/savedvideo', 'UserSavedVideoController@index');
+    $router->get('/user/historyvideo', 'UserHistoryVideoController@index');
 
     // Show Routes
     $router->get('user/{id}', 'UserController@show');
@@ -30,6 +32,8 @@ $router->group(['prefix'=>'v1'], function() use($router){
     $router->get('user/follower/{id}', 'UserFollowerController@show');
     $router->get('user/likevideo/{id}', 'UserLikeVideoController@show');
     $router->get('user/dislikevideo/{id}', 'UserDislikeVideoController@show');
+    $router->get('user/savedvideo/{id}', 'UserSavedVideoController@show');
+    $router->get('user/historyvideo/{id}', 'UserHistoryVideoController@show');
 
     // Store Routes
     $router->post('/user/store', 'UserController@create');
@@ -37,6 +41,8 @@ $router->group(['prefix'=>'v1'], function() use($router){
     $router->post('/user/follower/store', 'UserFollowerController@create');
     $router->post('/user/likevideo/store', 'UserLikeVideoController@create');
     $router->post('/user/dislikevideo/store', 'UserDislikeVideoController@create');
+    $router->post('/user/savedvideo/store', 'UserSavedVideoController@create');
+    $router->post('/user/historyvideo/store', 'UserHistoryVideoController@create');
 	
 	// Search Routes
 	$router->post('/user/search', 'UserController@search');
@@ -44,6 +50,8 @@ $router->group(['prefix'=>'v1'], function() use($router){
 	$router->post('/user/follower/search', 'UserFollowerController@search');
 	$router->post('/user/likevideo/search', 'UserLikeVideoController@search');
 	$router->post('/user/dislikevideo/search', 'UserDislikeVideoController@search');
+	$router->post('/user/savedvideo/search', 'UserSavedVideoController@search');
+	$router->post('/user/historyvideo/search', 'UserHistoryVideoController@search');
 
 	// Update Routes
 	$router->put('/user/update/{id}', 'UserController@update');
@@ -51,6 +59,8 @@ $router->group(['prefix'=>'v1'], function() use($router){
 	$router->put('/user/follower/update/{id}', 'UserFollowerController@update');
 	$router->put('/user/likevideo/update/{id}', 'UserLikeVideoController@update');
 	$router->put('/user/dislikevideo/update/{id}', 'UserDislikeVideoController@update');
+	$router->put('/user/savedvideo/update/{id}', 'UserSavedVideoController@update');
+	$router->put('/user/historyvideo/update/{id}', 'UserHistoryVideoController@update');
 
 	// Delete Routes
 	$router->delete('/user/delete/{id}', 'UserController@destroy');
@@ -58,5 +68,7 @@ $router->group(['prefix'=>'v1'], function() use($router){
 	$router->delete('/user/follower/delete/{id}', 'UserFollowerController@destroy');
 	$router->delete('/user/likevideo/delete/{id}', 'UserLikeVideoController@destroy');
 	$router->delete('/user/dislikevideo/delete/{id}', 'UserDislikeVideoController@destroy');
+	$router->delete('/user/savedvideo/delete/{id}', 'UserSavedVideoController@destroy');
+	$router->delete('/user/historyvideo/delete/{id}', 'UserHistoryVideoController@destroy');
 
 });
