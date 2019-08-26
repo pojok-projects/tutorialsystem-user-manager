e59a3e373d05214ec1ca1db7cf0a2ecd207c90b9
vibi <vibi94@gmail.com>
2019-08-27 01:05:36 +0700

    coba aja

--- app/Http/Controllers/UserFollowingController.php
+++ app/Http/Controllers/UserFollowingController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use Illuminate\Support\Str;
 use App\Traits\CheckDuplicate;
+use App\Traits\UserActivity;
 
 class UserFollowingController extends Controller
 {
     use CheckDuplicate;
+    use UserActivity;
 
     private $client;
     private $endpoint;
@@ -19,27 +22,26 @@ class UserFollowingController extends Controller
         $this->endpoint = env('ENDPOINT_API');
     }
 
-    public function index()
-    {
-    	$result = $this->client->request('GET', $this->endpoint.'user/following');
+    // public function index()
+    // {
+    // 	$result = $this->client->request('GET', $this->endpoint.'user/following');
 
-        if ($result->getStatusCode() != 200) {
-            return response()->json([
-                'status' => [
-                    'code' => $result->getStatusCode(),
-                    'message' => 'Bad Gateway',
-                ]
-            ], $result->getStatusCode());
-        }
+    //     if ($result->getStatusCode() != 200) {
+    //         return response()->json([
+    //             'status' => [
+    //                 'code' => $result->getStatusCode(),
+    //                 'message' => 'Bad Gateway',
+    //             ]
+    //         ], $result->getStatusCode());
+    //     }
 
-        return response()->json(json_decode($result->getBody(), true));
-    }
+    //     return response()->json(json_decode($result->getBody(), true));
+    // }
 
-    public function create(Request $request)
+    public function create($id, Request $request)
     {
         $rules = [
-            'user_id' => 'required',
-            'following_user_id' => 'required',
+            'following_user_id' => 'required|uuid'
         ];
 
         $customMessages = [
@@ -47,33 +49,42 @@ class UserFollowingController extends Controller
         ];
         $this->validate($request, $rules, $customMessages);
 
-        //Check duplicate by USER_ID and FOLLOWING_USER_ID
-        $query_check_duplicete = urlencode('"user_id='.$request->user_id.',following_user_id='.$request->following_user_id.'"');
-        $check_duplicate = $this->ReqCheck($query_check_duplicete, 'following');
+        //Check duplicate by FOLLOWING_USER_ID
+        // $query_check_duplicete = urlencode('"user_id='.$request->user_id.',following_user_id='.$request->following_user_id.'"');
+        // $check_duplicate = $this->ReqCheck($query_check_duplicete, 'following');
 
-        if ($check_duplicate['response'] === 500) {
-            return response()->json([
-                'status' => [
-                    'code' => $check_duplicate['response'],
-                    'message' => 'Bad Gateway',
-                ]
-            ], 500);
-        }
+        // if ($check_duplicate['response'] === 500) {
+        //     return response()->json([
+        //         'status' => [
+        //             'code' => $check_duplicate['response'],
+        //             'message' => 'Bad Gateway',
+        //         ]
+        //     ], 500);
+        // }
 
-        if ($check_duplicate['response']) {
-            return response()->json([
-                'status' => [
-                    'code' => '409',
-                    'message' => 'Duplicate',
-                    'total' => count($check_duplicate['result']),
-                ],
-                'result' => $check_duplicate['result'],
-            ], 409);
-        }else{
-            $result = $this->client->request('POST', $this->endpoint.'user/following/store', [
+        // if ($check_duplicate['response']) {
+        //     return response()->json([
+        //         'status' => [
+        //             'code' => '409',
+        //             'message' => 'Duplicate',
+        //             'total' => count($check_duplicate['result']),
+        //         ],
+        //         'result' => $check_duplicate['result'],
+        //     ], 409);
+        // }else{
+
+            $last_activity = json_decode($this->LastActivity($id, 'following'));
+
+            $array_following = array([
+                'id'                => (string) Str::uuid(),
+                'following_user_id' => $request->following_user_id,
+                'created_at'        => date(DATE_ATOM),
+                'updated_at'        => date(DATE_ATOM)
+            ]);
+
+            $result = $this->client->request('POST', $this->endpoint.'user/update/'.$id, [
                 'form_params' => [
-                    'user_id' => $request->user_id,
-                    'following_user_id' => $request->following_user_id,
+                    'following' => array_merge($last_activity, $array_following)
                 ]
             ]);
             
@@ -87,12 +98,12 @@ class UserFollowingController extends Controller
             }
 
             return response()->json(json_decode($result->getBody(), true));
-        }
+        // }
     }
 
     public function show($id)
     {
-        $result = $this->client->request('GET', $this->endpoint.'user/following/'.$id);
+        $result = $this->client->request('GET', $this->endpoint.'user/'.$id);
 
         if ($result->getStatusCode() != 200) {
             return response()->json([
@@ -103,7 +114,20 @@ class UserFollowingController extends Controller
             ], $result->getStatusCode());
         }
 
-        return response()->json(json_decode($result->getBody(), true));
+        $raw_user = json_decode($result->getBody(), true);
+
+        $following_data = array(
+            'status' => [
+                'code' => $result->getStatusCode(),
+                'message' => 'list query has been performed, data has been found',
+                'total' => ($raw_user['following'] ? count($raw_user['following']) : 0)
+            ],
+            'result' => ($raw_user['following'] ? $raw_user['following'] : [])
+        );
+
+        return response()->json($following_data);
+
+        // return response()->json(json_decode($result->getBody(), true));
     }
 
     public function search(Request $request)
